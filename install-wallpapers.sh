5e156c608f93835838f4a3bb48f1af582ededb98
Just Said <97128346+saidofficial@users.noreply.github.com>
2022-12-04 18:29:28 +0500

    wallpaper installer script added

---
+++ install-wallpapers.sh
@@ -0,0 +1,23 @@
+
+
+#Setup target directory
+targetDir = "/wallpapers"
+
+function get_random_photo() {
+	#Returns random file from target directory
+	files = ( "$targetDir"/* )
+	echo "${files[RANDOM % ${#files[@]}]}"
+
+}
+
+function setBackground() {
+	bg="$="
+	echo "Setting wallpaper to $bg"
+
+	#gsettings set org.gnome.desktop.background picture-uri "file://$bg"
+	#todo - write universal script
+}
+
+background=$(get_random_photo)
+echo "Next background is $background"
+setBackground $background
